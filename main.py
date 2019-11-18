20863b0c425cc31b6d07706dffc0774d368e3cfa
jt <jthomp@protonmail.com>
2019-11-18 08:26:18 -0600

    setting up api for backend

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
-from flask import Flask
+from flask import Flask, request
+import json
+from secrets import SystemRandom
+
+from wordlists import wordlist1
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return "hello, world"
+def dice_roll():
+    key = [SystemRandom().randrange(1, 7) for p in range(0, 5)]
+    keyInt = ''.join(map(str, key))
+    return int(keyInt)
+
+
+@app.route('/passphrase', methods=['POST'])
+def word_return():
+    passphrase = []
+    count = 1
+    if request.method == 'POST':
+        word_count = request.get_json(force=True)['word_count']
+
+        while count <= word_count:
+            count += 1
+            word = wordlist1.dice[dice_roll()]
+            passphrase.append(word)
+    return {'password': passphrase}
+
+
+if __name__ == ("__main__"):
+    app.run(debug=True)
