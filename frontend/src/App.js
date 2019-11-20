ec16e7f0e117013eb3cd46bc24feb95a48cb51d1
jt <jthomp@protonmail.com>
2019-11-19 23:46:58 -0600

    no need for useeffect, loading by button now

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import fetchPass from './fetchPass';
 
 const App = () => {
   const [pass, setPass] = useState('');
+  const [fetching, setFetching] = useState(false);
+
   var data = { word_count: 4, seperator: '_' };
 
-  useEffect(() => {
+  const getPass = () => {
+    setFetching(true);
     fetchPass(data).then(res => {
       res.text().then(data => {
+        setFetching(false);
         setPass(JSON.parse(data).password);
       });
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  };
   return (
     <div>
-      <h1>{pass}</h1>
+      {!fetching ? <h1>{pass}</h1> : <h1>Loading...</h1>}
+      <button onClick={getPass}>Get Password</button>
     </div>
   );
 };
