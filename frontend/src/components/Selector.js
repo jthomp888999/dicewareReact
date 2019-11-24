a27221d87c31ec26c8efc619797d44abede45ad2
jt <jthomp@protonmail.com>
2019-11-24 01:46:41 -0600

    removed uneeded useState hooks

--- frontend/src/components/Selector.js
+++ frontend/src/components/Selector.js
@@ -1,10 +1,7 @@
-import React, { useState } from 'react';
-import { Container, Segment, Grid, Form } from 'semantic-ui-react';
+import React from 'react';
+import { Grid, Form } from 'semantic-ui-react';
 
 const Selector = props => {
-  const [wordNum, setWordNum] = useState();
-  const [seperator, setSeperator] = useState();
-
   const { countState, sepState } = props;
 
   return (
@@ -13,7 +10,6 @@ const Selector = props => {
         <Form.Input
           type='number'
           min={1}
-          value={wordNum}
           onChange={e => countState(e.target.value)}
           label='Password word count: '
           placeholder='Amount of words in your password'
@@ -21,7 +17,6 @@ const Selector = props => {
       </Grid.Column>
       <Grid.Column>
         <Form.Input
-          value={seperator}
           onChange={e => sepState(e.target.value)}
           label='Password seperator: '
           placeholder='Any seperator to go between words'
